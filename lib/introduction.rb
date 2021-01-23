ab1a9a6f6d9398169741a43f9f0c7a6bc31ce7f0
Jabair Khan <jabairk@gmail.com>
2021-01-23 20:01:20 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -2,6 +2,9 @@ def introduction (name)
   puts "Hi, my name is #{name}."
 end
 
+def introduction_with_language_optional (name,language)
+  puts "Hi, my name is #{name} and I am learning to code in #{language}."
+end
 
 
 
